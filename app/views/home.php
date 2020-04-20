f6792c069f66fb9043c4c61c7e4b26f3649694ae
Jesus Hernan <jesushernan3@gmail.com>
2020-04-19 22:16:02 -0300

    terminado maqueta servicios

--- app/views/home.php
+++ app/views/home.php
@@ -3,16 +3,18 @@
   include 'includes/head.php';
 ?>
 <main class="container">
-  <section class="manifiesto">
+  <section class="manifiesto module">
     <h2>A higher plain</h2>
     <p>Creamos marcas y hermosos sitios web para clientes que necesitan un poco de ayuda para crear algo realmente especial. Lo hacemos con amor en Brighton para clientes de todo el mundo y también podemos hacerlo por usted ...</p>
   </section>
 
-  <section id="servicios">
+  <section id="servicios" class="module">
     <div class="row d-flex-centered f-direction-column">
-      <h2>Our Services</h2>
-      <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi minima porro earum at labore, placeat vitae in eaque alias, sint pariatur deleniti, rerum soluta et explicabo corporis? Excepturi, reiciendis animi.</p>
+      <h2 class="module-title">Our Services</h2>
+      <div class="caja-parrafo">
+        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi minima porro earum at labore, placeat vitae in eaque alias, sint pariatur deleniti, rerum soluta et explicabo corporis? Excepturi, reiciendis animi.</p>
+      </div>
     </div>
     <div class="row">
       <div class="col-25 d-flex-centered f-direction-column servicio">
@@ -51,8 +53,34 @@
     </div>
   </section>
 
-  <section id="contacto">
-  <p>contnido de contacto</p>
+  <section id="contacto" class="module">
+  <h2 class="module-title">Contacto</h2>
+    <div class="caja-formulario">
+    <div class="field">
+      <label class="label">Nombre</label>
+      <div class="control">
+      <input class="input" type="text" placeholder="e.g Alex Smith">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Email</label>
+      <div class="control">
+        <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Mensaje</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="Tu mensaje aqui"></textarea>
+      </div>
+    </div>
+    <div class="field">
+      <div class="control">
+        <button class="button is-link">Mandar Mensaje</button>
+      </div>
+    </div>
+  </form>
+    </div>
   </section>
 
 </main>

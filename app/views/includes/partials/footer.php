fefb3d755e1a02218fda7e875b192ab25bc0c56a
Jesus Hernan <jesushernan3@gmail.com>
2020-04-20 23:46:21 -0300

    mas footer

--- app/views/includes/partials/footer.php
+++ app/views/includes/partials/footer.php
@@ -1,8 +1,21 @@
 <footer class="container">
-  <div class="footer-top d-flex-centered">
-    <section>Contacto</section>
-    <section>Acerca</section>
+  <div class="footer-top">
+    <section class="col-50 contact-widget">
+      <h4>Contacto</h4>
+      <ul>
+        <li>Locacion</li>
+        <li>Tel</li>
+        <li>Email</li>
+      </ul>
+    </section>
+    <section class="col-50">
+      <h4>Acerca</h4>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit quo quas nihil quia eum omnis natus necessitatibus fugiat minus, excepturi voluptatem error nemo molestias deleniti suscipit, soluta perspiciatis doloremque asperiores.</p>
+    </section>
     <!-- <div class="logo-footer title">Desarrollo para todos</div> -->
   </div>
-  <div class="footer-bottom d-flex-centered">copyrigth 2020 - crmode</div>
+  <div class="footer-bottom d-flex-centered f-direction-column clearfix container"><p>
+    copyrigth 2020 - crmode
+  </p>
+</div>
 </footer>

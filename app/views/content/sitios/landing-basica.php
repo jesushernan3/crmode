58fedc6f90a97e64e79c6dd42f03fbf033c47b67
Jesus Hernan <jesushernan3@gmail.com>
2020-04-24 20:16:35 -0300

    mas product

--- app/views/content/sitios/landing-basica.php
+++ app/views/content/sitios/landing-basica.php
@@ -7,27 +7,39 @@
 <section class="manifiesto module">
     <h1>Landing PAge</h1>
     <h2>Básica</h2>
-    <p>Esta opción es la ideal para salir al mundo de internet de la manera más rapida y económica.</p>
+    <p>Esta opción es la ideal para salir al mundo de internet de la manera más rapida y económica. Tenga en cuenta que solamente concierne de la adaptación de una plantilla consu información. si usted busca opciones más personalizadas como colores, letras especificas, efectos, etc. entonces puede elijir otra opción más avanzada</p>
   </section>
 
 
- <section class="modulo">
-  <h3>Este desarrollo incluye:</h3>
-  <h4>4 secciones</h4>
-    
-  <ul>
-    <li>Inicio</li>
-    <li>Hasta 6 fotos</li>
-    <li>Texto informativo</li>
-    <li>Informacion de contacto</li>
-    <li>Diseño Responsive <span class="text-explain">se ve bien en todos los dispositivos moviles</span></li>
-  </ul>
+ <section class="articulo-info modulo">
+  <div class="columns">
+    <div class="column is-half">
+      <h3>Este desarrollo incluye:</h3>
+      <ul>
+        <li>4 secciones
+          <ul>
+            <li>Inicio</li>
+            <li>Hasta 6 fotos</li>
+            <li>Texto informativo</li>
+            <li>Informacion de contacto</li>
+          </ul>
+        </li>
+        <li>Diseño Responsive <span class="text-explain">se ve bien en todos los dispositivos moviles</span></li>
+        <li>Links a todas tus redes sociales</li>
+      </ul>
+      
+      <h3>No incluye</h3>
+      <ul>
+        <li>Formulario de contacto</li>
+        <li>Menu</li>
+      </ul>
+    </div>
+    <div class="column is-half">
+      derecha
+    </div>
+  </div>
 
-  <h3>No incluye</h3>
-  <ul>
-    <li>Formulario de contacto</li>
-    <li>Menu</li>
-  </ul>
+ 
  </section>
 
 

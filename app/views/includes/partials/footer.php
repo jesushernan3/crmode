999742227bb31f02998183193d44032898a81e93
jesus hernan <jesushernan@gmail.com>
2021-02-19 10:19:58 -0300

    intento de efectos al modal

--- app/views/includes/partials/footer.php
+++ app/views/includes/partials/footer.php
@@ -4,21 +4,23 @@
       <h4>Contacto</h4>
       <ul>
         <li><span class="icon-map-pin"></span>Boedo, Ciudad de Buenos Aires <br>
-            Prov. de Buenos Aires, <br>
-            Argentina </li>
+          Prov. de Buenos Aires, <br>
+          Argentina </li>
         <li><span class="icon-phone"></span>+549 11 2171-5239</li>
         <li><span class="icon-envelope"></span>info@crmode.com</li>
       </ul>
     </section>
     <section class="column is-mobile text-widget">
       <h4>Acerca</h4>
-      <p>Somos un grupo de freelancers que decidieron brindar sus años de experiencia en el sector tecnológico y ponerlo a disposición del marcado. Nuestra meta es seguir creciendo individualmente pero no perder la capacidad de ofrecer nuestros servicios a la comunidad. Este proyecto nace con esa intención.</p>
-      
+      <p>Somos un grupo de freelancers que decidieron brindar sus años de experiencia en el sector tecnológico y ponerlo
+        a disposición del marcado. Nuestra meta es seguir creciendo individualmente pero no perder la capacidad de
+        ofrecer nuestros servicios a la comunidad. Este proyecto nace con esa intención.</p>
+
       <!-- Prueba de Ventana modal para formulario o lo que sea -->
       <div><button id="boton" class="button">Modal</button></div>
       <div class="modal" id="page-modal">
         <div class="modal-background"></div>
-        <div class="modal-content">
+        <div class="modal-content" id="contenido">
           <div class="field">
             <label class="label">Name</label>
             <div class="control">
@@ -39,8 +41,9 @@
     </section>
     <!-- <div class="logo-footer title">Desarrollo para todos</div> -->
   </div>
-  <div class="footer-bottom d-flex-centered f-direction-column clearfix container"><p>
-    copyrigth 2020 - crmode
-  </p>
-</div>
+  <div class="footer-bottom d-flex-centered f-direction-column clearfix container">
+    <p>
+      copyrigth 2020 - crmode
+    </p>
+  </div>
 </footer>
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,13 @@
 var button = document.getElementById("boton");
 var modal = document.getElementById("page-modal");
+var contenido = document.getElementById("contenido");
+var envoltorio = document.getElementById("envoltorio");
 var close = document.getElementsByClassName("modal-close")[0];
 
 boton.onclick = function () {
-  modal.style.display = "flex";
+  modal.style.display = "block";
+  contenido.className = "modal-content active";
+  envoltorio.className = "blur";
 };
 close.onclick = function () {
   modal.style.display = "none";
